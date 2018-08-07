a5fecbf0d4bbfe2c2289e6956489b8bfab5d091f
DavidLandup <thealduinmaster@gmail.com>
2018-08-07 18:33:23 +0200

    Added test cases

---
+++ AbleBioRealtimeEventStreaming/src/main/java/com/able/Event.java
@@ -0,0 +1,21 @@
+package com.able;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Event {
+    private long id;
+    private Date date;
+
+    public Event(long id, Date date) {
+        this.id = id;
+        this.date = date;
+    }
+
+    public Event() {
+
+    }
+}
